b484383902f9e5f6e57a89a9f12a70faa6c6913a
ShalimaShaijan <shalima.workproject@gmail.com>
2024-08-23 13:31:36 +0530

    23082024

--- MyCrudApp/models.py
+++ MyCrudApp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Employee(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    emp_name = models.CharField(max_length=25)
+    emp_email = models.EmailField()
+    emp_contact = models.CharField(max_length=20)
+    emp_role = models.CharField(max_length=50)
+    emp_salary = models.IntegerField()
+    image = models.ImageField(upload_to='images')
+    
+    def __str__(self):
+        return self.emp_name
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url 
+        except:
+            url = ''
+        
+        return url
+
+
--- MyCrudApp/views.py
+++ MyCrudApp/views.py
@@ -1,3 +1,75 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Employee
+from .forms import EmployeeForm
+from django.db.models import Q
 
-# Create your views here.
+###For Creating a Employee Record
+def CreateEmployee(request):
+    form = EmployeeForm()
+    
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('create-emp')
+    
+    context = {
+        'form' : form,
+    }
+    
+    return render(request,'HomePage.html',context)
+
+### For Listing the Recorded Employee through Search
+def SearchEmployee(request):
+    
+    ###employees_list = Employee.objects.all()
+    search_query = ""
+    
+    if request.GET.get('search_query'):
+        search_query = request.GET.get('search_query')
+        
+    employees = Employee.objects.filter(Q(emp_name__icontains=search_query) |
+                                        Q(emp_role__icontains=search_query) |
+                                        Q(emp_salary__icontains=search_query)
+                                        )
+    context = {
+        'employees' : employees,
+        'search_query' : search_query,
+    }
+    return render(request,'EmployeeList.html',context)
+
+### For to edit the recorded Employees Record
+def EditEmployee(request,pk):
+    
+    employee = Employee.objects.get(id=pk)
+    form = EmployeeForm(instance=employee)
+        
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, request.FILES, instance=employee)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('list-emp')
+        
+    context = {
+        'employee': employee,
+        'form': form,
+    }
+    
+    return render(request,'EmployeeEdit.html',context)
+
+###For deleting a Record
+def DeleteEmployee(request,pk):
+    
+    employee = Employee.objects.get(id=pk)
+    
+    if request.method == 'POST':
+        employee.delete()
+        return redirect('list-emp')
+    
+    context = {
+        'employee' : employee,
+    }
+    
+    return render(request,'EmployeeDelete.html',context)
+        
